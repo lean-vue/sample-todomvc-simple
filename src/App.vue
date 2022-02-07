53c5eb372232ab080ae21fda2be6adba1c02466a
Micha Alt <mail@lean-stack.de>
2022-02-07 06:45:15 +0100

    Copy static template.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <div class="well">
-    <h1>{{ title }}</h1>
-  </div>
+  <h1>{{ title }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      title: 'Lean Vue App',
+      title: 'todos',
     };
   },
 };
 </script>
-
-<style scoped>
-h1 {
-  font-size: 24px;
-  font-weight: 500;
-}
-.well {
-  max-width: 720px;
-  margin: 4rem auto 0;
-  padding: 2rem;
-  background-color: #fafafa;
-  border: 1px solid #c0c0c0;
-  border-radius: 6px;
-}
-</style>
